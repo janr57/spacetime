1ca289055d211b3426363c8e8fc9239a364cbebd
janr <janr.devel@gmail.com>
2023-03-18 22:56:14 +0100

    Removing some *.aux *.el *.log *.toc, at last

--- auto/st-prologue.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "st-prologue"
- (lambda ()
-   (TeX-add-symbols
-    "scl")
-   (LaTeX-add-labels
-    "fig:prologue-spacetime"))
- :latex)
-
--- auto/st-topologicalmanifolds.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "st-topologicalmanifolds"
- (lambda ()
-   (TeX-add-symbols
-    "scl"))
- :latex)
-
